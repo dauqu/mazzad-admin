d529758fce4000413901df912feb3d8ddfc990b0
Dauqu <sujeetsingh734321@gmail.com>
2022-08-31 12:58:59 +0530

    Changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,14 +3,16 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "#1A2027",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: "start",
   color: theme.palette.text.secondary,
   borderRadius: 0,
+  color: "#ffffff",
 }));
 
 export default function Dashboard() {
@@ -23,7 +25,12 @@ export default function Dashboard() {
               minWidth: "200px",
             }}
           >
-            xs=8
+            <Typography variant="h6" gutterBottom>
+              Total active users
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+              51,234
+            </Typography>
           </Item>
         </Grid>
         <Grid item xs>
@@ -32,7 +39,12 @@ export default function Dashboard() {
               minWidth: "200px",
             }}
           >
-            xs=4
+            <Typography variant="h6" gutterBottom>
+              Total inactive users
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+              1,234
+            </Typography>
           </Item>
         </Grid>
         <Grid item xs>
@@ -41,7 +53,12 @@ export default function Dashboard() {
               minWidth: "200px",
             }}
           >
-            xs=4
+            <Typography variant="h6" gutterBottom>
+              Upcoming Products
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+              1,234
+            </Typography>
           </Item>
         </Grid>
         <Grid item xs>
@@ -50,7 +67,26 @@ export default function Dashboard() {
               minWidth: "200px",
             }}
           >
-            xs=8
+            <Typography variant="h6" gutterBottom>
+              Completed Products
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+              1,234
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs>
+          <Item
+            sx={{
+              minWidth: "200px",
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Total Vendors
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+              1,234
+            </Typography>
           </Item>
         </Grid>
       </Grid>
