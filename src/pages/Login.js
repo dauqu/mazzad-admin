1d526c8bd13968027ca041ed3989445803e0c261
Dauqu <sujeetsingh734321@gmail.com>
2022-09-01 18:16:49 +0530

    Changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,14 @@
-import PropTypes from "prop-types";
-import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import Grid from "@mui/material/Grid";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import { visuallyHidden } from "@mui/utils";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import CloseIcon from "@mui/icons-material/Close";
 import React, { useState } from "react";
-import axios from "axios";
-import MuiAlert from "@mui/material/Alert";
-import Snackbar from "@mui/material/Snackbar";
-import Stack from "@mui/material/Stack";
-import ModeEditTwoToneIcon from "@mui/icons-material/ModeEditTwoTone";
-import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
-import { Link } from "react-router-dom";
 import ShieldTwoToneIcon from "@mui/icons-material/ShieldTwoTone";
+import "../styles/login.css";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -48,11 +21,25 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Login() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={0}>
-        <Grid item xs={3}>
+      <Grid container spacing={0} sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#1A2027",
+        
+      }}>
+        <Grid item xs={3} sx={{
+          minWidth: "300px",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1A2027",
+        }}>
           <Item
             sx={{
-              minWidth: "200px",
               height: "100%",
               display: "flex",
               flexDirection: "column",
@@ -63,7 +50,6 @@ export default function Login() {
           >
             <Box
               sx={{
-                minWidth: "400px",
                 width: "80%",
                 height: "auto",
                 flexDirection: "column",
@@ -132,7 +118,6 @@ export default function Login() {
               />
               <Button
                 type="submit"
-                fullWidth
                 variant="contained"
                 color="primary"
                 sx={{
@@ -179,7 +164,7 @@ export default function Login() {
             </Box>
           </Item>
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={9} className="bg-image">
           <Item
             sx={{
               backgroundImage: `url("https://source.unsplash.com/random/800x600")`,
@@ -187,9 +172,26 @@ export default function Login() {
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
               height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            xs=4
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{ fontSize: "50px", fontWeight: "bold", color: "#fff" }}
+            >
+              Mazzad is a secure and easy to use web application
+            </Typography>
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{ fontSize: "50px", fontWeight: "bold", color: "#fff" }}
+            >
+              for managing auctions.
+            </Typography>
           </Item>
         </Grid>
       </Grid>
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,10 +1,191 @@
-function Pages() {
-    return (
-      <div>
-        <h1>Pages</h1>
-      </div>
-    );
-  }
-  
-  export default Pages;
-  
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import { Divider } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import ModeEditTwoToneIcon from "@mui/icons-material/ModeEditTwoTone";
+import Chip from "@mui/material/Chip";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#1A2027",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  borderRadius: 1,
+}));
+
+export default function Pages() {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    console.log("click");
+  };
+
+  //get pages
+  const [pages, setPages] = React.useState([]);
+  const getPageData = async () => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/pages`)
+      .then((res) => {
+        setPages(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  React.useEffect(() => {
+    getPageData();
+  }, []);
+
+  //Delete Page by ID
+  const handleDelete = (id) => {
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_URL}/pages/${id}`)
+      .then((res) => {
+        console.log(res);
+        setPages(pages.filter((page) => page._id !== id));
+        getPageData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //Update Published Status
+  const [published, setPublished] = React.useState(false);
+  const handlePublish = (_id, published) => {
+    axios
+      .patch(`${process.env.REACT_APP_BACKEND_URL}/pages/published/${_id}`, {
+        published: !published,
+      })
+      .then((response) => {
+        getPageData();
+        console.log(!published);
+        setPublished(response.data);
+      });
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, marginTop: 3 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense" sx={{ background: "#333" }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => navigate("/pages/create")}
+          >
+            {/* <CloseIcon /> */}
+            <AddIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Static Pages
+          </Typography>
+          <Divider sx={{ flexGrow: 1 }} />
+        </Toolbar>
+      </AppBar>
+
+      <Grid container spacing={1} alignItems="stretch">
+        <Grid item xs={12} alignItems="stretch">
+          <Item
+            onDoubleClick={handleClick}
+            sx={{
+              height: "86vh",
+              overflowY: "scroll",
+            }}
+          >
+            {/* Loop */}
+            <Grid container spacing={1}>
+              {pages.map((page) => (
+                <Grid item xs={3} key={page._id}>
+                  <Item
+                    sx={{
+                      backgroundColor: "#000",
+                      color: "#fff",
+                      textAlign: "start",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }} gutterBottom>
+                        {page._id}
+                      </Typography>
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        sx={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          whiteSpace: "nowrap",
+                          lineClamp: 1,
+                        }}
+                      >
+                        {page.title}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          mt: 2,
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          whiteSpace: "nowrap",
+                          lineClamp: 1,
+                        }}
+                      >
+                        {/* {page.published ? "Published" : "Draft"} */}
+                        {page.description}
+                      </Typography>
+                      <Typography variant="body2">
+                        {page.createdAt.split("T")[0]}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <IconButton
+                        size="small"
+                        onClick={() => handleDelete(page._id)}
+                        sx={{
+                          color: "#fff",
+                        }}
+                      >
+                        <DeleteTwoToneIcon />
+                      </IconButton>
+                      {/* <IconButton
+                        size="small"
+                        sx={{
+                          color: "#fff",
+                        }}
+                      >
+                        <ModeEditTwoToneIcon />
+                      </IconButton> */}
+                      <Chip
+                        onClick={() => handlePublish(page._id || page.published)}
+                        sx={{ ml: 2, cursor: "pointer" }}
+                        label={page.published ? "Published" : "Draft"}
+                        variant="outlined"
+                        color={page.published ? "success" : "error"}
+                        size="small"
+                      />
+                    </CardActions>
+                  </Item>
+                </Grid>
+              ))}
+            </Grid>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
