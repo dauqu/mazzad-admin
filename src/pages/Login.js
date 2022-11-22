33bb938712bd478dde8be667df1628314995b4d3
Harsh Singh <sujeetsingh734321@gmail.com>
2022-11-22 16:21:30 +0530

    Changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -46,7 +46,7 @@ export default function Login() {
   const createPost = (e) => {
     setIsLoading(true);
     e.preventDefault();
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, { withCredentials: true }, {
       email: username,
       password: password,
     })
