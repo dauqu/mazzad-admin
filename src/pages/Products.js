de7ca357c3d166e6a06d37b342b68d30e7b11b33
Harsh Singh <sujeetsingh734321@gmail.com>
2022-12-27 16:58:51 +0530

    Chnages

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -695,8 +695,7 @@ export default function Products() {
                               color: "#fff",
                             }}
                           >
-                            {/* {row.featured_image} */}
-                            https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg
+                            {row.image}
                           </Typography>
                         </HtmlTooltip>
                       </TableCell>
